95ff5ca0bb9878fc52e23298f198aea8cbca5c01
shopabro <92827872+shopabro@users.noreply.github.com>
2021-11-02 22:58:25 +1000

    Add files via upload

---
+++ home5/AppData.java
@@ -0,0 +1,25 @@
+package home5;
+
+
+
+public class AppData {
+
+    private String[] headers;
+    private Integer[][] data;
+
+    public String[] getHeaders() {
+        return headers;
+    }
+
+    public void setHeaders(String[] headers) {
+        this.headers = headers;
+    }
+
+    public Integer[][] getData() {
+        return data;
+    }
+
+    public void setData(Integer[][] data) {
+        this.data = data;
+    }
+}
